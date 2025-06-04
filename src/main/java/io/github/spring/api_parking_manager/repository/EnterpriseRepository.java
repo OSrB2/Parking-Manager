dc6a7c46f5808b5bfb030e4c57f83b17f78faa89
OSrB2 <pedro_oliva@outlook.com.br>
2025-06-04 16:06:25 -0300

    feat: create repositories

---
+++ src/main/java/io/github/spring/api_parking_manager/repository/EnterpriseRepository.java
@@ -0,0 +1,11 @@
+package io.github.spring.api_parking_manager.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.spring.api_parking_manager.model.EnterpriseModel;
+
+public interface EnterpriseRepository extends JpaRepository<EnterpriseModel, UUID> {
+  
+}
---
+++ src/main/java/io/github/spring/api_parking_manager/repository/VehicleRepository.java
@@ -0,0 +1,11 @@
+package io.github.spring.api_parking_manager.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.spring.api_parking_manager.model.VehicleModel;
+
+public interface VehicleRepository extends JpaRepository<VehicleModel, UUID>{
+  
+}
