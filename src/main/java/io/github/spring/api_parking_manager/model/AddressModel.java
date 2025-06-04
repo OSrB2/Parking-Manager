b57c8b0df68552ec204eed7e825f2cc7ef6e707e
OSrB2 <pedro_oliva@outlook.com.br>
2025-06-04 16:49:14 -0300

    refact: address embedded to enterprise model

--- src/main/java/io/github/spring/api_parking_manager/model/AddressModel.java
+++ src/main/java/io/github/spring/api_parking_manager/model/AddressModel.java
@@ -1,23 +1,9 @@
 package io.github.spring.api_parking_manager.model;
 
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.Embeddable;
 
-@Entity
-@Table(name = "tb_vehicle")
-@Data
-@EntityListeners(AuditingEntityListener.class)
-
+@Embeddable
 public class AddressModel {
 
   @Column(name = "street_address", nullable = false)
@@ -31,12 +17,4 @@ public class AddressModel {
 
   @Column(name = "postal_code", nullable = false)
   private String postalCode;
-
-  @CreatedDate
-  @Column(name = "created_at", nullable = false, updatable = false)
-  private LocalDateTime createdAt;
-
-  @LastModifiedDate
-  @Column(name = "updated_at", nullable = false)
-  private LocalDateTime updatedAt;
 }
