bdea8380ec7769379058b11b52324aef0e782163
OSrB2 <pedro_oliva@outlook.com.br>
2025-06-04 10:56:21 -0300

    basic settings

---
+++ src/main/java/io/github/spring/api_parking_manager/ApiParkingManagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.spring.api_parking_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/spring/api_parking_manager/ApiParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spring.api_parking_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
