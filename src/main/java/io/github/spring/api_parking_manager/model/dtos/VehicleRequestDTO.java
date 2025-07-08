ecb6325217fdae334d2aba8d252442c13c16660d
OSrB2 <pedro_oliva@outlook.com.br>
2025-07-08 18:08:18 -0300

    refac: refact dto and mapper

--- src/main/java/io/github/spring/api_parking_manager/model/dtos/VehicleRequestDTO.java
+++ src/main/java/io/github/spring/api_parking_manager/model/dtos/VehicleRequestDTO.java
@@ -2,6 +2,7 @@ package io.github.spring.api_parking_manager.model.dtos;
 
 import io.github.spring.api_parking_manager.model.Vehicle;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record VehicleRequestDTO(
@@ -9,7 +10,7 @@ public record VehicleRequestDTO(
   @Size(min = 3, max = 50, message = "Must have between 3 and 50 characters!")
   String brand,
   @NotBlank(message = "Is required!")
-  @Size(min = 3, max = 50, message = "Must have between 3 and 50 characters!")
+  @Size(min = 2, max = 50, message = "Must have between 3 and 50 characters!")
   String model,
   @NotBlank(message = "Is required!")
   @Size(min = 3, max = 25, message = "Must have between 3 and 25 characters!")
@@ -17,6 +18,6 @@ public record VehicleRequestDTO(
   @NotBlank(message = "Is required!")
   @Size(min = 7, max = 7, message =  "Must have exaclty 7 characters!")
   String plate,
-  @NotBlank(message = "Is required!")
+  @NotNull(message = "Is required!")
   Vehicle type
 ) {}
--- src/main/java/io/github/spring/api_parking_manager/model/mappers/VehicleMapper.java
+++ src/main/java/io/github/spring/api_parking_manager/model/mappers/VehicleMapper.java
@@ -4,13 +4,14 @@ import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 import io.github.spring.api_parking_manager.model.VehicleModel;
+import io.github.spring.api_parking_manager.model.dtos.VehicleRequestDTO;
 import io.github.spring.api_parking_manager.model.dtos.VehicleResponseDTO;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface VehicleMapper {
 
-  VehicleModel toEntity(VehicleResponseDTO vehicleResponseDTO);
+  VehicleModel toEntity(VehicleRequestDTO vehicleResquestDTO);
 
   VehicleResponseDTO toResponseDTO(VehicleModel vehicleModel);
 } 
