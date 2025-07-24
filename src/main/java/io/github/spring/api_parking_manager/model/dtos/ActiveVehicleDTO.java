50c37889bb057b0a368d9ea28bd6fa65e69e1448
OSrB2 <pedro_oliva@outlook.com.br>
2025-07-24 13:12:17 -0300

    feat: create ActiveVehicleDTO to report parkeds

---
+++ src/main/java/io/github/spring/api_parking_manager/model/dtos/ActiveVehicleDTO.java
@@ -0,0 +1,25 @@
+package io.github.spring.api_parking_manager.model.dtos;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
+
+import io.github.spring.api_parking_manager.service.Utils.DateFormatterUtil;
+
+public record ActiveVehicleDTO(
+  String plate,
+  String model,
+  String brand,
+  String type,
+  LocalDateTime entry
+) {
+
+  public String formattedDate() {
+    return "entry" + DateFormatterUtil.format(this.entry);
+  }
+
+  @JsonGetter("entry")
+  public String getEntryFormmatted() {
+    return DateFormatterUtil.format(this.entry);
+  }
+}
