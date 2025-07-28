7ecb89e3b8e31df55ce6edf6dec8c40cee06e99b
OSrB2 <pedro_oliva@outlook.com.br>
2025-07-28 11:16:56 -0300

    Refac: Remove Date Formatter

--- src/main/java/io/github/spring/api_parking_manager/model/dtos/ActiveVehicleDTO.java
+++ src/main/java/io/github/spring/api_parking_manager/model/dtos/ActiveVehicleDTO.java
@@ -2,24 +2,13 @@ package io.github.spring.api_parking_manager.model.dtos;
 
 import java.time.LocalDateTime;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-
-import io.github.spring.api_parking_manager.Utils.DateFormatterUtil;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public record ActiveVehicleDTO(
   String plate,
   String model,
   String brand,
   String type,
+  @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
   LocalDateTime entry
-) {
-
-  public String formattedDate() {
-    return "entry" + DateFormatterUtil.format(this.entry);
-  }
-
-  @JsonGetter("entry")
-  public String getEntryFormmatted() {
-    return DateFormatterUtil.format(this.entry);
-  }
-}
+) {}
--- src/main/java/io/github/spring/api_parking_manager/model/dtos/VehicleReportDTO.java
+++ src/main/java/io/github/spring/api_parking_manager/model/dtos/VehicleReportDTO.java
@@ -2,10 +2,14 @@ package io.github.spring.api_parking_manager.model.dtos;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public record VehicleReportDTO(
   String plate,
   String type,
+  @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
   LocalDateTime entry,
+  @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
   LocalDateTime exit,
   String lengthStay
 ) {}
