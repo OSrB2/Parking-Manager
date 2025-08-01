0f0df6f89eb5e78f43a2d7430df48e067b162ced
OSrB2 <pedro_oliva@outlook.com.br>
2025-08-01 11:34:58 -0300

    refac configs

--- src/test/java/io/github/spring/api_parking_manager/ApplicationTests.java
+++ src/test/java/io/github/spring/api_parking_manager/ApplicationTests.java
@@ -2,6 +2,7 @@ package io.github.spring.api_parking_manager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class ApplicationTests {
