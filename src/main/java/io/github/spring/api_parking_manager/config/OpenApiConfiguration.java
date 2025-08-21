796a7d0b4f6ccab100568f3df7060f67b0c66b42
Pedro Oliveira <pedro_oliva@outlook.com.br>
2025-08-21 11:41:30 -0300

    feat: adding security scheme for bearer auth

--- src/main/java/io/github/spring/api_parking_manager/config/OpenApiConfiguration.java
+++ src/main/java/io/github/spring/api_parking_manager/config/OpenApiConfiguration.java
@@ -3,8 +3,12 @@ package io.github.spring.api_parking_manager.config;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
 @OpenAPIDefinition(
@@ -15,7 +19,17 @@ import io.swagger.v3.oas.annotations.info.Info;
       name = "Pedro Oliveira",
       email = "pedro_oliva@outlook.com.br"
     )
-  )
+  ),
+  security = {
+    @SecurityRequirement(name = "bearerAuth")
+  }
+)
+@SecurityScheme(
+  name = "bearerAuth",
+  type = SecuritySchemeType.HTTP,
+  bearerFormat = "JWT",
+  scheme = "bearer",
+  in = SecuritySchemeIn.HEADER
 )
 public class OpenApiConfiguration {
   
